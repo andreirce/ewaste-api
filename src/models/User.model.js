07c4441bc457682778d327b7f76909cd6c6f146b
Stéphanie Cândido <stephanie.candido42@gmail.com>
2024-08-14 18:40:29 -0300

    feat: encripta senha dos users

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -1,52 +1,70 @@
-import { DataTypes } from 'sequelize'
-import { Address } from './Address.model.js';
-import { database } from '../database/connection.js';
+import { User } from '../models/User.model.js';
+import bcrypt from 'bcrypt';
 
-export const User = database.define('User',
-  {
-    user_id: {
-      type: DataTypes.UUID,
-      primaryKey: true,
-      defaultValue: DataTypes.UUIDV4
-    },
+const saltRounds = 10;
 
-    name: {
-      type: DataTypes.STRING(255),
-      allowNull: false
-    },
+export const createUser = async (req, res) => {
+    try {
+        const { name, email, password, cpf, cnpj, phone, address_id } = req.body;
 
-    email: {
-      type: DataTypes.STRING(255),
-      allowNull: false
-    },
+        // Criptografar a senha
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
 
-    password: {
-      type: DataTypes.STRING(255),
-      allowNull: false
-    },
+        await User.sync();
+        const newUser = await User.create({ name, email, password: hashedPassword, cpf, cnpj, phone, address_id });
+        return res.status(201).json({ newUser });
 
-    cpf: {
-      type: DataTypes.CHAR(11), 
-    },
-
-    cnpj: {
-      type: DataTypes.CHAR(14),
-    },
-
-    phone: {
-      type: DataTypes.CHAR(15),
-    },
-
-    address_id: {
-      type: DataTypes.UUID,
-      allowNull: false,
-      references: {
-        model: Address,
-        key: 'id'
-      }
+    } catch (error) {
+        return res.status(400).json({ messageError: error.message });
     }
-  },
-  {
-    tableName: 'User'
-  }
-);
+}
+
+export const getAllUser = async (req, res) => {
+    try {
+        const users = await User.findAll();
+        return res.json({ users });
+
+    } catch (error) {
+        return res.status(400).json({ messageError: error.message });
+    }
+}
+
+export const deleteUser = async (req, res) => {
+    const { id } = req.params;
+    await User.destroy({
+        where: {
+            id
+        }
+    });
+
+    return res.status(200).json({ message: 'Usuário deletado com sucesso' });
+}
+
+export const updateUser = async (req, res) => {
+    const { id } = req.params;
+    const { name, email, password, cpf, cnpj, phone } = req.body;
+
+
+    let hashedPassword;
+    if (password) {
+        hashedPassword = await bcrypt.hash(password, saltRounds);
+    }
+
+    await User.update(
+        {
+            name,
+            email,
+            password: hashedPassword || Sequelize.literal('password'),
+            cpf,
+            cnpj,
+            phone
+        },
+        {
+            where: {
+                id
+            }
+        }
+    );
+
+    return res.status(200).json({ message: 'Usuário editado com sucesso' });
+}
