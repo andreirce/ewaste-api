93eb8e3124cf206df6b386a3c081a755b73548d3
Stéphanie Cândido <stephanie.candido42@gmail.com>
2024-08-13 23:16:25 -0300

    update server

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import corsConfig from './config/cors-config.js';
 import { userRouter } from './routes/user.routes.js';
 import { couponRouter } from './routes/coupon.routes.js';
 import { partnerRouter } from './routes/partner.routes.js';
@@ -6,25 +7,21 @@ import { addressRouter } from './routes/address.routes.js';
 import { itemRouter } from './routes/item.routes.js';
 import { categoryRouter } from './routes/category.routes.js';
 import { pointsRouter } from './routes/collectionpoints.routes.js';
-import { categoryRouter } from './routes/category.routes.js';
-
-
 
 
 const app = express();
 const port = 4000
+
+app.use(corsConfig);
 
 app.use(express.json());
 app.use(userRouter);
 app.use(couponRouter);
 app.use(partnerRouter);
 app.use(addressRouter);
+app.use(categoryRouter);
 app.use(itemRouter);
 app.use(pointsRouter)
-app.use(categoryRouter);
-
-
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
